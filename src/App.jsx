1a9a93b975aca53059f17cb1937867cb87da6af2
neha <nsharma215@gmail.com>
2024-12-10 22:11:21 +0000

    fix: infinte loop fix for use()

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import "./App.css";
 import { Suspense } from "react";
 import Users from "./examples/Users";
-import Theme from './examples/Theme';
-import Action from './examples/Action';
+import Theme from "./examples/Theme";
+import Action from "./examples/Action";
 import Optimistic from "./examples/Optimistic";
 import FormStatus from "./examples/FormStatus";
 import FormState from "./examples/FormState";
 
+const fetchPosts = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  return res.json();
+};
+
 function App() {
+  const fetchPostsPromise = fetchPosts();
   return (
     <>
       <div>
@@ -16,11 +22,12 @@ function App() {
             <h1 className="text-2xl text-center font-bold mt-5">Loading...</h1>
           }
         >
+          <Users fetchPostsPromise={fetchPostsPromise} />
           {/* <Action /> */}
-          <FormState />
-         {/* <FormStatus /> */}
-           {/* <Optimistic /> */}
-          {/* <Users /> */}
+          {/* <FormState /> */}
+          {/* <FormStatus /> */}
+          {/* <Optimistic /> */}
+          
           {/* <Theme /> */}
         </Suspense>
       </div>
